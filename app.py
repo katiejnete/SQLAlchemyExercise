493c63d610aa6420353a583e4c04ad5ecba1ccb1
katiejnete <156715616+katiejnete@users.noreply.github.com>
2024-03-09 01:13:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 """Blogly application."""
 
 from flask import Flask, render_template, redirect, request
-from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -9,8 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = '38432084'
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-debug = DebugToolbarExtension(app)
 
 connect_db(app)
 app.app_context().push()
